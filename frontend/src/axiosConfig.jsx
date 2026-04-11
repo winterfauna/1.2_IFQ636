c2edae362bc49122ee08bf04dd6a748e0abebf7c
Winter <winteracrobat@gmail.com>
2026-04-12 02:09:19 +1000

    update axios

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  // baseURL: 'http://localhost:5001', // local
+  baseURL: 'http://localhost:5001', // local
   baseURL: 'http://3.26.64.186:5001', // live
   headers: { 'Content-Type': 'application/json' },
 });
